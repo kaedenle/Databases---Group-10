a825b71c9f31da78c9fc4aa4a644ac84f35527a4
Caitlin <caitlinfstar@gmail.com>
2022-11-25 13:48:05 -0500

    Form

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -10,12 +10,12 @@ import Button from "react-bootstrap/Button";
 
 function Register() {
   return (
-    <Container>
+    <Container className="mw-100">
       <Row>
-        <Col>
+        <Col sm={7}>
           <LeftHomeSection />
         </Col>
-        <Col>
+        <Col sm={5} className="right_side">
           <RightHomeSection />
         </Col>
       </Row>
@@ -25,19 +25,19 @@ function Register() {
 
 function LeftHomeSection() {
   return (
-    <div>
-      <h1>Survey</h1>
-      <p>
-        Hello This is the left section of the page. This will be a brief
-        description of the survey website.
+    <div className="d-flex flex-column align-items-center justify-content-center">
+      <h1 className="text-center">Survey</h1>
+      <p className="text-center">
+        Hello This is the left section of the page.
+        <br /> This will be a brief description of the survey website.
         <br />
         Section
-        <ul>
-          <li>Hello</li>
-          <li>Hello</li>
-          <li>Hello</li>
-        </ul>
       </p>
+      <ul>
+        <li>Hello</li>
+        <li>Hello</li>
+        <li>Hello</li>
+      </ul>
     </div>
   );
 }
@@ -46,31 +46,34 @@ function RightHomeSection() {
   return (
     <div>
       {/* Register User */}
-      <Form>
-        <Form.Group className="mb-3" controlId="formBasicFirst">
-          <Form.Label>First Name</Form.Label>
-          <Form.Control type="text" placeholder="First Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicLast">
-          <Form.Label>Last Name</Form.Label>
-          <Form.Control type="text" placeholder="Last Name" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicUsername">
-          <Form.Label>Username</Form.Label>
-          <Form.Control type="text" placeholder="Username" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formBasicPassword">
-          <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
-        </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
-        </Button>
-      </Form>
+      <h2 class="startTitle">Let's Get Started</h2>
+      <div className="d-flex justify-content-center">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicFirst">
+            <Form.Label>First Name</Form.Label>
+            <Form.Control size="1x" type="text" placeholder="First Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicLast">
+            <Form.Label>Last Name</Form.Label>
+            <Form.Control size="1x" type="text" placeholder="Last Name" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicUsername">
+            <Form.Label>Username</Form.Label>
+            <Form.Control size="1x" type="text" placeholder="Username" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control size="1x" type="email" placeholder="Enter email" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control size="1x" type="password" placeholder="Password" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
     </div>
   );
 }
