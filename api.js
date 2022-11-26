494b4b0f974d724e3ded1d36a5bc6c5d35946460
kaedenle <kaedenle@gmail.com>
2022-11-26 05:35:05 -0500

    Added endpoints to the backend. Added endpoints include: -get_users   *Gets all fields from a given user by their username (can remove password)   *FIELD(S): userName -get_survey   *Gets all fields from a given survey by their title and owner's username in 'info' field   *Gets all questions from a given survey in 'questions' field   *FIELD(S): title, userName -login   *Given username and password, validates the user's credientials (returns userID if successful)   *FIELD(S): userName, password -get_question_list   *Gets all questions from a given survey   *FIELD(S): title, userName -get_results   *Gets results from a given question   *FIELD(S): questionID
    Added nodemon to the backend

--- api.js
+++ api.js
@@ -1,14 +1,210 @@
-
-
 require('express');
 require('mysql2');
 exports.setApp = function ( app, client )
 {
-    app.post('/api/login', async (req, res, next) =>
+//------------------------------------------------------------FUNCTIONS------------------------------------------------------------
+    const get_user = async (req, res, next) =>
     {
-        const [result] = await client.query('SELECT * FROM Person');
-        console.log(result);
+        //IN - userName
+
+        //error messages
+        if(!req.body.userName)
+        {
+            var ret = {error: "ERROR: Invalid userName field"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //query
+        var search = [req.body.userName];
+        const [result] = await client.query('SELECT * FROM Users WHERE userName = ?', search);
+
+        //validation check
+        if(result.length == 0){
+            var ret = {error: "ERROR: Username not found"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //assumes there's only one of each username, returns one made first if multiple
+        res.locals.user = {info: result[0]};
+        req.body.userID = result[0].userID
+        next();
+        return
+    }
+
+    const get_survey = async (req, res, next) =>
+    {
+        //IN - Title, userID
+        var title = req.body.title;
+        var userID = req.body.userID;
+
+        //error messages
+        if(!req.body.title)
+        {
+            var ret = {error: "ERROR: Invalid survey field provided"} 
+            res.status(200).json(ret);
+            return
+        }
+        if(!req.body.userID)
+        {
+            var ret = {error: "ERROR: Invalid userID field provided"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //query
+        var search = [title, userID];
+        const [result] = await client.query('SELECT * FROM Surveys WHERE title = ? and creatorID = ?', search);
+
+        //validation check
+        if(result.length == 0){
+            var ret = {error: "ERROR: Invalid survey provided"} 
+            res.status(200).json(ret);
+            return
+        }
+        //assumes there's only one of each survey, returns one made first if multiple
+        res.locals.survey = {info: result[0]};
+        req.body.surveyID = result[0].surveyID;
+        next()
+        return
+    }
+//------------------------------------------------------------USER ENDPOINTS------------------------------------------------------------
+    app.post('/get_user', get_user, async (req, res, next) =>
+    {
+        var ret = res.locals.user;   
+        res.status(200).json(ret);
+    });
+
+    app.post('/login', async (req, res, next) =>
+    {
+        //IN - userName, password
+
+        //error messages
+        if(!req.body.userName)
+        {
+            var ret = {error: "ERROR: Invalid userName field provided"} 
+            res.status(200).json(ret);
+            return
+        }
+        if(!req.body.password)
+        {
+            var ret = {error: "ERROR: Invalid password field provided"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //query
+        var search = [req.body.userName, req.body.password];
+        const [result] = await client.query('SELECT * FROM Users WHERE userName = ? AND password = ?', search);
+        if(result.length == 0){
+            //err = "ERROR: Invalid credentials"
+            var ret = {error: "ERROR: Invalid credentials"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //assumes there's only one of each username, returns one made first if multiple
+        var ret = {userID: result[0].userID};
+        res.status(200).json(ret);
+    });
+//------------------------------------------------------------SURVEY ENDPOINTS------------------------------------------------------------
+    app.post('/get_survey', get_user, get_survey, async (req, res, next) => {
+        //IN - title, userName
+        var ret = res.locals.survey;
+        res.status(200).json(ret);
+    });
+
+//------------------------------------------------------------QUESTION ENDPOINTS------------------------------------------------------------
+    /*app.post('/get_question', async (req, res, next) =>
+    {
+        //IN - questionID
+        var id = "";
+
+        if(String(req.body.questionID))
+            id = req.body.questIONID
+
+        var search = [id];
+        const [result] = await client.query('SELECT * FROM Questions WHERE questionID = ?', search);
+        if(result.length == 0){
+            var ret = {error: "ERROR: Invalid id provided"} 
+            res.status(200).json(ret);
+            return
+        }
+        //assumes there's only one of each question, returns one made first if multiple
+        var ret = {info: result[0]};
+        res.status(200).json(ret);
+    });*/
+
+    app.post('/get_question_list', get_user, get_survey, async (req, res, next) =>
+    {
+        //IN - title, useName
+
+        //get from get_survey
+        var search = [req.body.surveyID];
+        //var serach = [title, userID]
+        const [result] = await client.query('SELECT * FROM Questions WHERE surveyID = ?', search);
+        //const [result] = await client.query('SELECT * FROM Surveys WHERE title = ? AND userID = ?', search);
+        /*if(result.length == 0){
+            var ret = {error: "ERROR: No questions in this survey"} 
+            res.status(200).json(ret);
+            return
+        }*/
+        //returns all questions
         var ret = {info: result};
         res.status(200).json(ret);
     });
+//------------------------------------------------------------RESULTS ENDPOINTS------------------------------------------------------------
+    app.post('/get_results', async (req, res, next) =>
+    {
+        //IN - questionID
+
+        //error messages
+        if(!req.body.questionID)
+        {
+            var ret = {error: "ERROR: Invalid survey field provided"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        //query
+        var search = [req.body.questionID];
+        const [result] = await client.query('SELECT * FROM Questions WHERE questionID = ?', search);
+        
+        //get table needed from type
+        var type = result[0].type;
+        var type_str = "";
+        switch(type){
+            case 1:
+                type_str = "Type1_answers"
+                break;
+            case 2:
+                type_str = "Type2_answers"
+                break;
+            default:
+                var ret = {error: "ERROR: Invalid type provided"} 
+                res.status(200).json(ret);
+                return
+        }
+
+        //query the answer table
+        search = [req.body.questionID]
+        const [result_] = await client.query('SELECT * FROM ' + type_str + ' WHERE questionID = ?', search);
+
+        //validation check
+        if(result_.length == 0){
+            var ret = {error: "ERROR: No questions found"} 
+            res.status(200).json(ret);
+            return
+        }
+
+        var ret = {info: result_[0]};
+        res.status(200).json(ret);
+    });
+    /*
+    app.get('/', async (req, res, next) =>
+    {
+        res.render("hello");
+    });'*/
+
 }
--- mySQL.js
+++ mySQL.js
@@ -1,20 +1,23 @@
 const mysql = require('mysql2')
 
 async function main(){
-    const connection = mysql.createConnection({
+  /*const connection = mysql.createConnection({
         host: 'localhost',
         user: 'root',
         password: 'Password',
         database: 'test'
-      }).promise();
-      /*const connection = mysql.createConnection({
-        host: 'ubuntu-s-1vcpu-2gb-nyc1-01',
-        user: 'ethan',
-        password: 'testing12',
-        database: 'survey_project_1'
-      });*/
-    //connection.connect();
-    var [results] = await connection.query("SELECT * FROM Person");
-    console.log(results);
+      }).promise();*/
+  const connection = mysql.createConnection({
+    host: "161.35.126.112",
+    user: "universal",
+    password: "password",
+    database: "survey_project_1",
+  })
+  .promise();
+  var obj = {userName: "Tarova"}
+  var [results] = await connection.query("SELECT * FROM Questions")
+  //var [results] = await connection.query("INSERT INTO Users (firstName, lastName, userName, email, userID, password) VALUES ('Kaeden', 'Le', 'kaedenle', 'kaedenle@gmail.com', 'b0c79de6-6a27-11ed-91a4-62bd4a1ad21c', 'password');");
+  console.log(results);
+  connection.end()
 }
 main();
