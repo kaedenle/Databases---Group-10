4af902f22c2c18b772e9773b99b862aebf0c3f7c
Caitlin Fabian <caitlinfstar@knights.ucf.edu>
2022-11-29 02:07:57 -0500

    Home page

--- frontend/src/pages/CreateSurvey.jsx
+++ frontend/src/pages/CreateSurvey.jsx
@@ -1,8 +1,27 @@
-import React from 'react';
+import e from 'cors';
+import React, { useState } from 'react';
+
+//Bootstrap
+import Form from 'react-bootstrap/Form';
+//css
+import '../css/create.css';
 
 function CreateSurvey() {
+  const [title, setTitle] = useState(false);
+  const [name, setName] = useState('Title');
+  let data = [
+    {
+      id: 1,
+      name: 'test 1',
+    },
+    {
+      id: 2,
+      name: 'test 2',
+    },
+  ];
   return (
     <div className="create">
+      {/* <h1 className="title">{title}</h1> */}
       <p>Hello</p>
     </div>
   );
--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 
+//Bootstrap
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+import thumbnail from '../assets/survey-thumbnail.png';
+
 // CSS
 import '../App.css';
 import '../css/home.css';
@@ -7,7 +14,44 @@ import '../css/home.css';
 function Home() {
   return (
     <div className="home">
-      <h1> Home</h1>
+      <h1 className="home-title"> Active Title</h1>
+      <hr className="header" />
+      <Container fluid>
+        {/* This is where You will adjust to make it dynamic and add data of survey
+        This is for basic format */}
+        <Row className="add-space">
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img
+              src={thumbnail}
+              className="thumbnail"
+              alt="survey thumbnail"
+            />
+          </Col>
+          <Col style={{ marginTop: '20px' }}>
+            <h3>Survey Title</h3>
+            <p> This will be the description of the survey</p>
+          </Col>
+        </Row>
+        <Row className="add-space">
+          <Col
+            md={3}
+            className="text-center"
+          >
+            <img
+              src={thumbnail}
+              className="thumbnail"
+              alt="survey thumbnail"
+            />
+          </Col>
+          <Col style={{ marginTop: '20px' }}>
+            <h3>Survey Title</h3>
+            <p> This will be the description of the survey</p>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
